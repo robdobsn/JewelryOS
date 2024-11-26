3e6269e46b7f9006e252c58fe9daaf9dd6c33c1e
Rob Dobson <rob@dobson.com>
2024-11-26 17:09:23 +0000

    Added new SysType for Heart v1.5

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
-FROM espressif/idf:v5.2.1
+FROM espressif/idf:v5.3.1
 WORKDIR /project
 RUN apt-get update && apt-get install -y g++
---
+++ systypes/Heart1.5/features.cmake
@@ -0,0 +1,42 @@
+# Set the target Espressif chip
+set(IDF_TARGET "esp32c3")
+
+# System version
+add_compile_definitions(SYSTEM_VERSION="2.1.0")
+
+# Heart type jewelry
+add_compile_definitions(FEATURE_HEART_JEWELRY)
+
+# Enable power control function setup - this will keep the power on indefinitely if
+# other power control functions are not enabled
+add_compile_definitions(FEATURE_POWER_CONTROL_SETUP)
+
+# Enable sleeping between animations
+# add_compile_definitions(FEATURE_ENABLE_SLEEP_MODE)
+
+# Enable power control function check user shutdown - this will check the power
+# button for user input to shutdown the device
+# add_compile_definitions(FEATURE_POWER_CONTROL_USER_SHUTDOWN)
+
+# Enable power control function for low battery shutdown
+# add_compile_definitions(FEATURE_POWER_CONTROL_LOW_BATTERY_SHUTDOWN)
+
+# Enable heart LED animations
+add_compile_definitions(FEATURE_HEART_ANIMATIONS)
+
+# Enable MAX30101 sensor and stand-alone I2C bus
+# add_compile_definitions(FEATURE_MAX30101_SENSOR)
+# add_compile_definitions(FEATURE_I2C_STANDALONE)
+
+# Add I2C bus in main
+add_compile_definitions(FEATURE_REGISTER_I2C_BUS_IN_MAIN)
+
+# Raft components
+set(RAFT_COMPONENTS
+    RaftSysMods@main
+    RaftI2C@main
+)
+
+# File system
+set(FS_TYPE "littlefs")
+set(FS_IMAGE_PATH "../Common/FSImage")
